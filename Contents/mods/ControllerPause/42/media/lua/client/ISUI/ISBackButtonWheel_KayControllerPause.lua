72522a7c67abdeb74041095d6663ec653f098f58
Kaydeth <Kaydeth@gmail.com>
2025-06-22 09:14:09 -0400

    Added missing function to fill back button wheel when time is paused

--- Contents/mods/ControllerPause/42/media/lua/client/ISUI/ISBackButtonWheel_KayControllerPause.lua
+++ Contents/mods/ControllerPause/42/media/lua/client/ISUI/ISBackButtonWheel_KayControllerPause.lua
@@ -1,5 +1,96 @@
 require "ISUI/ISBackButtonWheel"
 
+function ISBackButtonWheel:addCommands()
+	local playerObj = getSpecificPlayer(self.playerNum)
+	
+	self:center()
+
+	self:clear()
+
+	-- local isPaused = UIManager.getSpeedControls() and UIManager.getSpeedControls():getCurrentGameSpeed() == 0
+	isPaused = false
+
+	if isPaused then
+		self:addSlice(nil, nil, nil)
+		self:addSlice(nil, nil, nil)
+	else
+		if not ISBackButtonWheel.disablePlayerInfo then
+			self:addSlice(getText("IGUI_BackButton_PlayerInfo"), getTexture("media/ui/Heart2_On.png"), self.onCommand, self, "PlayerInfo")
+		else
+			self:addSlice(nil, nil, nil)
+		end
+		if not ISBackButtonWheel.disableCrafting then
+			self:addSlice(getText("IGUI_BackButton_Crafting"), getTexture("media/ui/Carpentry_On.png"), self.onCommand, self, "Crafting")
+			self:addSlice(getText("IGUI_BackButton_Building"), getTexture("media/ui/Build_Tool.png"), self.onCommand, self, "Building")
+		else
+			self:addSlice(nil, nil, nil)
+		end
+	end
+
+	if getCore():isZoomEnabled() and not getCore():getAutoZoom(self.playerNum) then
+		if ISBackButtonWheel.disableZoomIn then
+			self:addSlice(nil, nil, nil)
+		else
+			self:addSlice(getText("IGUI_BackButton_Zoom", getCore():getNextZoom(self.playerNum, -1) * 100), getTexture("media/ui/ZoomIn.png"), self.onCommand, self, "ZoomMinus")
+		end
+	end
+
+	if UIManager.getSpeedControls() and not isClient() then
+		if ISBackButtonWheel.disableTime then
+			self:addSlice(nil, nil, nil)
+			self:addSlice(nil, nil, nil)
+		else
+			if UIManager.getSpeedControls():getCurrentGameSpeed() == 0 or getGameTime():getTrueMultiplier() > 1 then
+				self:addSlice(getText("IGUI_BackButton_Play"), getTexture("media/ui/speedControls/Play_Off.png"), self.onCommand, self, "Pause")
+			else
+				self:addSlice(getText("UI_optionscreen_binding_Pause"), getTexture("media/ui/speedControls/Pause_Off.png"), self.onCommand, self, "Pause")
+			end
+	
+			local multiplier = getGameTime():getTrueMultiplier()
+			if multiplier == 1 or multiplier == 40 then
+				self:addSlice(getText("IGUI_BackButton_FF1"), getTexture("media/ui/speedControls/FFwd1_Off.png"), self.onCommand, self, "FastForward")
+			elseif multiplier == 5 then
+				self:addSlice(getText("IGUI_BackButton_FF2"), getTexture("media/ui/speedControls/FFwd2_Off.png"), self.onCommand, self, "FastForward")
+			elseif multiplier == 20 then
+				self:addSlice(getText("IGUI_BackButton_FF3"), getTexture("media/ui/speedControls/Wait_Off.png"), self.onCommand, self, "FastForward")
+			end
+		end
+	end
+
+	if Core.isLastStand() then
+		self:addSlice(getText("IGUI_BackButton_LastStand"), Joypad.Texture.AButton, self.onCommand, self, "LastStand")
+	end
+
+	if getCore():isZoomEnabled() and not getCore():getAutoZoom(self.playerNum) then
+		if ISBackButtonWheel.disableZoomOut then
+			self:addSlice(nil, nil, nil)
+		else
+			self:addSlice(getText("IGUI_BackButton_Zoom", getCore():getNextZoom(self.playerNum, 1) * 100), getTexture("media/ui/ZoomOut.png"), self.onCommand, self, "ZoomPlus")
+		end
+	end
+
+	if not isPaused and not playerObj:getVehicle() and not ISBackButtonWheel.disableMoveable then
+		self:addSlice(getText("IGUI_BackButton_Movable"), getTexture("media/ui/Furniture_Off2.png"), self.onCommand, self, "MoveFurniture")
+	else
+		self:addSlice(nil, nil, nil)
+	end
+
+	local searchManager = ISSearchManager.getManager(playerObj);
+	if not isPaused and searchManager and not ISBackButtonWheel.disableScavenge then
+		if searchManager.isSearchMode then
+			self:addSlice(getText("UI_disable_search_mode"), getTexture("media/ui/foraging/eyeconOff.png"), self.onCommand, self, "ForageMode");
+			searchManager:checkCloseIcons();
+			for _, icon in pairs(searchManager.closeIcons) do
+				self:addSlice(getText("IGUI_Pickup") .. " " .. icon.itemObj:getDisplayName(), icon.itemTexture, self.onCommand, self, "ForageItem");
+				self:addSlice(getText("UI_foraging_DiscardItem") .. " " .. icon.itemObj:getDisplayName(), icon.itemTexture, self.onCommand, self, "DiscardForageItem");
+				break; --only add the first icon found
+			end;
+		else
+			self:addSlice(getText("UI_enable_search_mode"), getTexture("media/ui/foraging/eyeconOn.png"), self.onCommand, self, "ForageMode");
+		end;
+	end;
+end
+
 function ISBackButtonWheel:onCommand(command)
 	local focus = nil
 	-- local isPaused = UIManager.getSpeedControls() and UIManager.getSpeedControls():getCurrentGameSpeed() == 0
